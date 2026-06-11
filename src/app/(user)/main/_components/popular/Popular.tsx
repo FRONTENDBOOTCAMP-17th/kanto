eab11f716a657eb23fe7708ae608d26cdf2e8450
DoHyuk-Centric <clzlsdlwhgdk12@gmail.com>
2026-06-11 10:08:37 +0900

    feat: 각페이지 카드 컴포넌트 제작 및 supabase 조회

--- src/app/(user)/main/_components/popular/Popular.tsx
+++ src/app/(user)/main/_components/popular/Popular.tsx
@@ -1,12 +1,60 @@
+import { getPopularList } from "@/services/main/main";
+import { formatTimeAgo } from "@/utils/formatTime";
 import PopularList from "./PopularList";
 
-export default function Popular() {
+export default async function Popular() {
+  const { usedGoods, rentals, jobs } = await getPopularList();
+
+  const usedGoodsItems = usedGoods
+    .filter((p) => p.used_goods[0] != null)
+    .slice(0, 4)
+    .map((p) => ({
+      id: p.id,
+      title: p.title,
+      price: p.used_goods[0].price,
+      location: p.used_goods[0].location_type,
+      likes: p.like_count,
+      time: formatTimeAgo(p.created_at),
+      popular: p.like_count >= 20,
+      imageSrc: (p.used_goods[0].images as string[] | null)?.[0],
+    }));
+
+  const rentalItems = rentals
+    .filter((p) => p.rentals[0] != null)
+    .slice(0, 4)
+    .map((p) => ({
+      id: p.id,
+      title: p.title,
+      price: p.rentals[0].price ?? 0,
+      location: p.rentals[0].location ?? "",
+      likes: p.like_count,
+      time: formatTimeAgo(p.created_at),
+      popular: p.like_count >= 20,
+      imageSrc: (p.rentals[0].images as string[] | null)?.[0],
+    }));
+
+  const jobItems = jobs
+    .filter((p) => p.jobs[0] != null)
+    .slice(0, 4)
+    .map((p) => ({
+      id: p.id,
+      title: p.title,
+      price: p.jobs[0].salary,
+      location: p.jobs[0].location_type,
+      likes: p.like_count,
+      time: formatTimeAgo(p.created_at),
+      popular: p.like_count >= 20,
+      imageSrc: (p.jobs[0].images as string[] | null)?.[0],
+    }));
+
   return (
     <>
       <div className="mt-8">
         <h1 className="text-3xl font-bold text-gray-900 mb-6 md:mb-10">인기 목록</h1>
       </div>
-      <PopularList />
+      <PopularList title="중고거래" items={usedGoodsItems} link={`/usedgoods`} />
+      <PopularList title="구인구직" items={jobItems} link={`job`} />
+      <PopularList title="방렌트" items={rentalItems} link={`rental`} />
     </>
   );
 }
--- src/app/(user)/main/_components/popular/PopularList.tsx
+++ src/app/(user)/main/_components/popular/PopularList.tsx
@@ -1,44 +1,28 @@
 import { ChevronRight } from "lucide-react";
 import MainCard, { type MainCardItem } from "../MainCard";
-import { getUsedGoodsList } from "@/services/usedGoods/usedGoods";
-import { formatTimeAgo } from "@/utils/formatTime";
+import Link from "next/link";
 
+interface PopularListProps {
+  title: string;
+  items: MainCardItem[];
+  link: string;
+}
 
-export default async function PopularList() {
-  const data = await getUsedGoodsList();
-
-  const items: MainCardItem[] = data
-    .filter((post) => post.used_goods[0] != null)
-    .slice(0, 4)
-    .map((post) => {
-    const goods = post.used_goods[0];
-    return {
-      id: post.id,
-      title: post.title,
-      price: goods.price,
-      location: goods.location_type,
-      likes: post.like_count,
-      time: formatTimeAgo(post.created_at),
-      popular: post.like_count >= 20,
-      imageSrc: (goods.images as string[] | null)?.[0],
-    };
-  });
-
+export default function PopularList({ title, items, link }: PopularListProps) {
   return (
-    <>
+    <section className="mb-10">
       <div className="flex justify-between items-center">
-        <h2 className="text-xl font-bold">중고거래</h2>
-        <button className="flex gap-1 cursor-pointer items-center text-teal-500 font-medium text-sm">
+        <h2 className="text-xl font-bold">{title}</h2>
+        <Link href={link}  className="flex gap-1 cursor-pointer items-center text-teal-500 font-medium text-sm">
           전체보기
           <ChevronRight className="w-4 h-4" />
-        </button>
+        </Link>
       </div>
-
       <div className="mt-3 flex flex-col gap-2 md:grid md:grid-cols-4 md:gap-4">
         {items.map((item) => (
           <MainCard key={item.id} item={item} />
         ))}
       </div>
-    </>
+    </section>
   );
 }
